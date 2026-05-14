18ebb5f74b033c363533741215cefd938fae62f8
Ayush Kumar <ak00945367@gmail.com>
2026-05-14 12:31:20 +0530

    fix api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://legal-intake-backend-1.onrender.com",
+    "https://legal-intake-backend-1.onrender.com/api",
 });
 
 export default API;
